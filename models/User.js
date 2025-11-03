01d5219c059b3d8d9dc535a60f6cb87078ec5ac6
ShivamFD <akkivishwakarma52@gmail.com>
2025-11-03 14:42:09 +0530

    User schema created for admin and employee

--- models/User.js
+++ models/User.js
@@ -1,1 +1,21 @@
-setup
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+
+const userSchema = new mongoose.Schema({
+name: { type: String, required: true },
+email: { type: String, required: true, unique: true },
+password: { type: String, required: true },
+role: { type: String, enum: ['admin', 'employee'], default: 'employee' },
+employeeRef: { type: mongoose.Schema.Types.ObjectId, ref: 'Employee' },
+createdAt: { type: Date, default: Date.now }
+});
+
+
+
+userSchema.methods.comparePassword = async function (candidate) {
+return bcrypt.compare(candidate, this.password);
+};
+
+
+module.exports = mongoose.model('User', userSchema);
